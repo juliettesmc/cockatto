2fbc501dd53f0e95defab2d8b6971ba03d786766
juliette <juliette2martinezc@gmail.com>
2023-01-08 12:19:26 -0600

    Project Setup and React Basics

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+let todoList = [
+  { title: "Complete assignment", ObjectID: 1 },
+  {
+    title: "Complete assignment",
+    ObjectID: 2,
+  },
+  {
+    title: "Complete assignment",
+    ObjectID: 3,
+  },
+];
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: " " }}>
+      <h1> Todo List </h1>
+
+      <hr />
+      <ul>
+        {todoList.map(function (item) {
+          return <li key={item.ObjectID}>{item.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
