abe36b7a5c54f85e8dc96107805f23fcfb044b6f
juliette <juliette2martinezc@gmail.com>
2023-02-17 08:56:02 -0600

    borrador

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 // import {TodoList} from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
-
 // import React from "react";
 
 function App() {
@@ -15,26 +14,41 @@ const [todoList, setTodoList] = useState(
   // cuando tiene mayusculas son componentes y minsculas variables,state
 );
 
-useEffect(() => {
-  const Promise =( (resolve, reject) => {
-setTimeout(function(){
-  // ()=>resolve({data:{JSON.parse(localStorage.getItem('todoList'))}})
-console.log ('2 second has passed')
-},2000)
-Promise.then(result=>{
-this.setState({todoList:result});
-})
+const [isLoading,setIsLoading]=useState(true);
 
-  .then(resolve=>resolve.JSON.parse(localStorage.getItem("todoList")))
-  .then(data=>console.log(data))
-   })
-   
-},[])
-// debajo del todoList estado, definir un nuevo useEffect gancho 
-// react con una list de dependencias vacia.
 useEffect(() => {
+  new Promise ((resolve, reject) => {
+    
+  setTimeout(()=>{ //establece un temporizador que ejecuta una función o una pieza de código 
+    resolve({
+      data:{
+       todoList:
+    JSON.parse(localStorage.getItem("todoList")),
+             },
+           });
+           console.log('2 second has passed')
+         }, 2000); //tiempo de retardo
+          }
+      ).then((result) => { //método de un Promiseobjeto toma hasta dos argumentos: funciones de devolución de llamada para los casos cumplidos y rechazados del Promise
+      console.log(result)
+      setTodoList([...result.data.todoList])
+      setIsLoading(false); 
+      console.log(result);
+  });
+}, []);
+
+
+useEffect(() => {
+
+// if (isLoading) {
+//  return <p>'Loading...'</p>
+// } else {
+//   return todoList
+// }
+
   localStorage.setItem("todoList", JSON.stringify(todoList));
-}, [todoList]); //dependent lo que hace es buscar.
+}, [todoList,isLoading]); //dependent lo que hace es buscar.
+
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
     console.log(todoList)
@@ -48,8 +62,8 @@ useEffect(() => {
     setTodoList(filteredArray);
   };
 
-
   return (
+    
     <>
       <h1> Todo List </h1>
       <AddTodoForm onAddTodo={addTodo} />  
@@ -63,3 +77,4 @@ useEffect(() => {
 
 export default App;
 // <> fragmentos.
+
