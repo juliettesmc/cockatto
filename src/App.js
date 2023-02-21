6aa471ff347fd195d57a65e2fedbcf19d90df6e1
juliette <juliette2martinezc@gmail.com>
2023-02-20 18:21:29 -0600

    example

--- src/App.js
+++ src/App.js
@@ -15,27 +15,65 @@ const [todoList, setTodoList] = useState(
 );
 
 const [isLoading,setIsLoading]=useState(true);
+const postTodo = async (todo) => {
+
+  try {
+
+     const airtableData = {
+        fields: {
+           name: todo
+        }
+     }
+
+     const response = await 
+        fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`, 
+        {
+           method: 'GET',
+           cache:'no-cache',
+           headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`
+           },
+           body: JSON.stringify(airtableData)
+    });
+
+    if (!response.ok) {
+      const message = `Error has ocurred:${response.status}`;
+      throw new Error(message);
+    }
+
+    const dataResponse = await response.json();
+    return dataResponse;
+
+  } catch (error) {
+
+        console.log(error.message);
+        return null;
+  }
+}
 
 useEffect(() => {
-  new Promise ((resolve, reject) => {
-    
-  setTimeout(()=>{ //establece un temporizador que ejecuta una función o una pieza de código 
-    resolve({
-      data:{
-       todoList:
-    JSON.parse(localStorage.getItem("todoList")),
-             },
-           });
-           console.log('2 second has passed')
-         }, 2000); //tiempo de retardo
-          }
-      ).then((result) => { //método de un Promiseobjeto toma hasta dos argumentos: funciones de devolución de llamada para los casos cumplidos y rechazados del Promise
-      console.log(result)
-      setTodoList([...result.data.todoList])
-      setIsLoading(false); 
-      // isLoading(false);
-      console.log(result);
-  });
+  postTodo()
+  setIsLoading()
+  
+  // new Promise ((resolve, reject) => {
+  // setTimeout(()=>{ //establece un temporizador que ejecuta una función o una pieza de código 
+  //   resolve({
+  //     data:{
+  //      todoList:
+  //   JSON.parse(localStorage.getItem("todoList")),
+  //            },
+  //          });
+  //          console.log('2 second has passed')
+  //        }, 2000); //tiempo de retardo
+  //         }
+  //     ).then((result) => { //método de un Promiseobjeto toma hasta dos argumentos: funciones de devolución de llamada para los casos cumplidos y rechazados del Promise
+  //     console.log(result)
+  //     setTodoList([...result.data.todoList])
+  //     setIsLoading(false); 
+  //     // isLoading(false);
+  //     console.log(result);
+  // });
 }, []);
 
 
