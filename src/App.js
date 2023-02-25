19ca5463a5069970d5467e2e9e628a4963c4f1c3
juliette <juliette2martinezc@gmail.com>
2023-02-25 10:59:34 -0600

    borrador

--- src/App.js
+++ src/App.js
@@ -6,88 +6,65 @@ import TodoList from "./TodoList";
 // import React from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState(
+    localStorage.getItem("todoList")
+      ? JSON.parse(localStorage.getItem("todoList"))
+      : []
+    // cuando tiene mayusculas son componentes y minsculas variables,state
+  );
 
-const [todoList, setTodoList] = useState(
-  localStorage.getItem("todoList")
-  ? JSON.parse(localStorage.getItem("todoList"))
-  : []
-  // cuando tiene mayusculas son componentes y minsculas variables,state
-);
+  const [isLoading, setIsLoading] = useState(true);
+  console.log(process.env);
+  useEffect(() => {
+    fetch(
+      `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+        },
+      }
+    )
+      .then((resp) => console.log(resp.json()))
+      .then((result) => {
+        console.log(result.records);
+          setTodoList([...result.records]); //result es mi variable
+          setIsLoading(false);
+          console.log(result.records);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
-const [isLoading,setIsLoading]=useState(true);
-const postTodo = async (todo) => {
+    // new Promise ((resolve, reject) => {
+    // setTimeout(()=>{ //establece un temporizador que ejecuta una función o una pieza de código
+    //   resolve({
+    //     data:{
+    //      todoList:
+    //   JSON.parse(localStorage.getItem("todoList")),
+    //            },
+    //          });
+    //          console.log('2 second has passed')
+    //        }, 2000); //tiempo de retardo
+    //         }
+    //     ).then((result) => { //método de un Promiseobjeto toma hasta dos argumentos: funciones de devolución de llamada para los casos cumplidos y rechazados del Promise
+    //     console.log(result)
+    //     setTodoList([...result.data.todoList])
+    //     setIsLoading(false);
+    //     // isLoading(false);
+    //     console.log(result);
+    // });
+  }, []);
 
-  try {
-
-     const airtableData = {
-        fields: {
-           name: todo
-        }
-     }
-
-     const response = await 
-        fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`, 
-        {
-           method: 'GET',
-           cache:'no-cache',
-           headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`
-           },
-           body: JSON.stringify(airtableData)
-    });
-
-    if (!response.ok) {
-      const message = `Error has ocurred:${response.status}`;
-      throw new Error(message);
+  useEffect(() => {
+    if (isLoading) {
+      localStorage.setItem("todoList", JSON.stringify(todoList));
     }
-
-    const dataResponse = await response.json();
-    return dataResponse;
-
-  } catch (error) {
-
-        console.log(error.message);
-        return null;
-  }
-}
-
-useEffect(() => {
-  postTodo()
-  setIsLoading()
-  
-  // new Promise ((resolve, reject) => {
-  // setTimeout(()=>{ //establece un temporizador que ejecuta una función o una pieza de código 
-  //   resolve({
-  //     data:{
-  //      todoList:
-  //   JSON.parse(localStorage.getItem("todoList")),
-  //            },
-  //          });
-  //          console.log('2 second has passed')
-  //        }, 2000); //tiempo de retardo
-  //         }
-  //     ).then((result) => { //método de un Promiseobjeto toma hasta dos argumentos: funciones de devolución de llamada para los casos cumplidos y rechazados del Promise
-  //     console.log(result)
-  //     setTodoList([...result.data.todoList])
-  //     setIsLoading(false); 
-  //     // isLoading(false);
-  //     console.log(result);
-  // });
-}, []);
-
-
-useEffect(() => {
-
-if (isLoading) {
-
-  localStorage.setItem("todoList", JSON.stringify(todoList));}
-
-}, [todoList,isLoading]); //dependent lo que hace es buscar.
+  }, [todoList, isLoading]); //dependent lo que hace es buscar.
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
-    console.log(todoList)
+    console.log(todoList);
   };
 
   const removeTodo = (id) => {
@@ -99,20 +76,17 @@ if (isLoading) {
   };
 
   return (
-    
     <>
       <h1> Todo List </h1>
-      <AddTodoForm onAddTodo={addTodo} />  
-       {/* agregandp el addtoodo pasansolo como props  */}
-      
-      {isLoading?(
-        "Loading"
-      ):(
+      <AddTodoForm onAddTodo={addTodo} />
+      {/* agregandp el addtoodo pasansolo como props  */}
 
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo} /> 
+      {isLoading ? (
+        "Loading"
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
       )}
-       {/* pasar removeTodo como accesorio de controlador de devolucion de llamadas */}
-       
+      {/* pasar removeTodo como accesorio de controlador de devolucion de llamadas */}
     </>
   );
 }
@@ -120,3 +94,4 @@ if (isLoading) {
 export default App;
 // <> fragmentos.
 
+// keyMyid97CToDfGBB
